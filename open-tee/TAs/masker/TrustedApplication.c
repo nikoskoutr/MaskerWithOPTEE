4e8941fbd44dfbbb7b145eb86323c5b4e6706117
Nikos <nikoskoutr@gmail.com>
2021-05-18 13:34:56 +0300

    fixed entrypoints for TEEs

--- open-tee/TAs/masker/TrustedApplication.c
+++ open-tee/TAs/masker/TrustedApplication.c
@@ -148,6 +148,34 @@ int masker(uint32_t param_types, TEE_Param * params) {
  * Mandatory TA functions.
  ******************************************************************************/
 
+TEE_Result TA_CreateEntryPoint(void)
+{
+     OT_LOG(LOG_ERR, "Open Session to TEE");
+    return TEE_SUCCESS;
+}
+
+void TA_DestroyEntryPoint(void)
+{
+     OT_LOG(LOG_ERR, "Destroy Session to TEE");
+}
+
+
+TEE_Result TA_OpenSessionEntryPoint(uint32_t param_types,
+        TEE_Param  params[4], void **sess_ctx)
+{
+
+    (void)&params;
+    (void)&sess_ctx;
+    return TEE_SUCCESS;
+}
+
+
+void TA_CloseSessionEntryPoint(void *sess_ctx)
+
+{
+    (void)&sess_ctx;
+}
+
 TEE_Result TA_InvokeCommandEntryPoint(void * sess_ctx, uint32_t cmd_id, uint32_t param_types, TEE_Param params[4]) 
 {
 	if(cmd_id == MASKER) { // TODO: ADD MASKER COMMAND ID
